0e1c7dc12585150d8f647a42a8506a4d2d429b7f
Kanchanasin <kanchanasin12@gmail.com>
2026-04-29 23:06:07 +0700

    Change const to let for currentMenu variable
    155

--- app/ropa/controller/page.tsx
+++ app/ropa/controller/page.tsx
@@ -152,7 +152,7 @@ export default function RoPARecordPage() {
   const [isRopaMenuOpen, setIsRopaMenuOpen] = useState(true);
 
   // Page States
-  const currentMenu: "Controller" | "Processor" = "Controller";
+  let currentMenu: "Controller" | "Processor" = "Controller";
   const [currentTab, setCurrentTab] = useState("Activities");
 
   // Modal States
